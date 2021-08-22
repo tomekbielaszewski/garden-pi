1d6373952924bb979bd1fd7251b8e03255b9385f
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2021-08-22 17:13:44 +0200

    typo

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ SERVICE="python"
 
 if pgrep -x "$SERVICE" >/dev/null
 then
-    echo "$SERVICE is running. I won't replace th crontab right now. Kill python processes first!"
+    echo "$SERVICE is running. I won't replace the crontab right now. Kill python processes first!"
 else
     export backup_date=`date +20%y%m%d-%H%M%S`
     echo "Backing up old crontab into cron.backup.$backup_date"
