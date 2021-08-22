52550d139b0c05f5bc938d1b2a72d1c208a1b18d
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2021-08-22 16:02:01 +0200

    removed test log

--- watering.py
+++ watering.py
@@ -3,9 +3,6 @@ import RPi.GPIO as GPIO
 import time
 import sys
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument used:', str(sys.argv)
-
 # This little script provides an easy way to open the electrical relays connected
 # to RPi using command:
 # python watering.py 60 0
