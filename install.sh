be9cdf2de3d4083b6420418fe228c16973b74273
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2021-08-22 17:10:01 +0200

    backup date printing fixed

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ then
     echo "$SERVICE is running. I won't replace th crontab right now. Kill python processes first!"
 else
     export backup_date=`date +20%y%m%d-%H%M%S`
-    echo 'Backing up old crontab into $backup_date'
+    echo "Backing up old crontab into $backup_date"
     crontab -l > cron.backup.${backup_date}
     echo "Installing new crontab"
     crontab < ./crontab.txt
