6e71dbbe8e620b49dbf1dd71106b7bf2584060f7
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2021-08-22 18:40:02 +0200

    backuping to directory

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ then
     echo "$SERVICE is running. I won't replace the crontab right now. Kill python processes first!"
 else
     export backup_date=`date +20%y%m%d-%H%M%S`
-    echo "Backing up old crontab into cron.backup.$backup_date"
+    echo "Backing up old crontab into backup directory"
     mkdir -p backup
     crontab -l > backup/cron.backup.${backup_date}
     echo "Installing new crontab"
