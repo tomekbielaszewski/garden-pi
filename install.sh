2b2b5001d561cc15ac002346a29750ac47a629f7
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2021-08-22 18:39:36 +0200

    backuping to directory

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ then
 else
     export backup_date=`date +20%y%m%d-%H%M%S`
     echo "Backing up old crontab into cron.backup.$backup_date"
+    mkdir -p backup
     crontab -l > backup/cron.backup.${backup_date}
     echo "Installing new crontab"
     crontab < ./crontab.txt
