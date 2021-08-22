0b10508fc9e6cbbee4059f53d91b374062ef0273
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2021-08-22 17:09:24 +0200

    crontab installation checks if python is already executed

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-export backup_date=`date +20%y%m%d-%H%M%S`
-crontab -l > cron.backup.${backup_date}
-crontab < ./crontab.txt
+
+SERVICE="python"
+
+if pgrep -x "$SERVICE" >/dev/null
+then
+    echo "$SERVICE is running. I won't replace th crontab right now. Kill python processes first!"
+else
+    export backup_date=`date +20%y%m%d-%H%M%S`
+    echo 'Backing up old crontab into $backup_date'
+    crontab -l > cron.backup.${backup_date}
+    echo "Installing new crontab"
+    crontab < ./crontab.txt
+fi
