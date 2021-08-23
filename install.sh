98d7669d67da4205f8881e04c5e2181766488f1b
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2021-08-23 10:05:04 +0200

    updated log when python already running

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ SERVICE="python"
 
 if pgrep -x "$SERVICE" >/dev/null
 then
-    echo "$SERVICE is running. I won't replace the crontab right now. Kill python processes first!"
+    echo "$SERVICE is running. I won't replace the crontab right now. Kill $SERVICE processes first!"
 else
     export backup_date=`date +20%y%m%d-%H%M%S`
     echo "Backing up old crontab into backup directory"
